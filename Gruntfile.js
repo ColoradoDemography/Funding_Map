9688abfab936fda2ef69484f6518205aa0f6e1fe
Daniel Trone <royhobbstn@yahoo.com>
2016-02-04 23:46:08 +0000

    master branch changes

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,7 +3,7 @@ module.exports = function(grunt) {
     grunt.initConfig({
 
         watch: {
-            files: ['js/app.js', 'Gruntfile.js'],
+            files: ['js/app.js', 'Gruntfile.js', 'index.html'],
             tasks: ['jshint', 'jsbeautifier']
         },
 
@@ -26,54 +26,17 @@ module.exports = function(grunt) {
                     'index.html': ['index.html']
                 }
             }
-        },
-
-
-        concat: {
-
-            js: {
-                src: ['jquery/jquery-2.1.4.min.js', 'jquery/jquery-ui.min.js', 'js/jQDateRangeSlider-min.js', 'js/bootstrap.min.js', 'js/typeahead.jquery.js', 'js/leaflet.js', 'js/oms.min.js', 'js/Leaflet.MakiMarkers.js', 'js/Leaflet.Modal.js', 'js/easy-button.js', 'js/leaflet.label.js', 'js/leaflet-geocoder-mapzen.min.js'],
-                dest: 'build/js/bundle.js',
-            }
-
-        },
-
-        uglify: {
-
-            js: {
-                src: 'build/js/bundle.js',
-                dest: 'build/js/bundle.min.js'
-            }
-
-        },
-
-        cssmin: {
-            options: {
-                keepSpecialComments: 0,
-                shorthandCompacting: false,
-                roundingPrecision: -1,
-                root: '',
-                rebase: 'false'
-            },
-            target: {
-                files: {
-                    'build/css/bundle.css': ['css/bootstrap.min.css', 'jquery/jquery-ui.min.css', 'jquery/jquery-ui.structure.min.css', 'jquery/jquery-ui.theme.min.css', 'css/classic-min.css', 'jquery/jquery-ui.css', 'css/leaflet.css', 'css/leaflet.modal.css', 'css/easy-button.css', 'css/leaflet-label.css', 'css/app.css']
-                }
-            }
         }
 
     });
 
 
     grunt.loadNpmTasks('grunt-prettify');
-    grunt.loadNpmTasks('grunt-contrib-cssmin');
-    grunt.loadNpmTasks('grunt-contrib-uglify');
-    grunt.loadNpmTasks('grunt-contrib-concat');
     grunt.loadNpmTasks('grunt-contrib-jshint');
     grunt.loadNpmTasks('grunt-contrib-watch');
     grunt.loadNpmTasks("grunt-jsbeautifier");
 
     //grunt.registerTask('default', ['cssmin']);
     grunt.registerTask('default', ['watch']);
-    grunt.registerTask('build', ['jshint', 'jsbeautifier', 'cssmin', 'concat', 'uglify']);
+
 };
